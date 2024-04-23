36a1087cbda5373374d6ff66d181e0bd59630d2b
rezabehesh138 <161194595+rezabehesh138@users.noreply.github.com>
2024-04-23 20:28:34 +0330

    Update Ultrasonic_Distance_Code.ino

--- Ultrasonic_Distance_Code.ino
+++ Ultrasonic_Distance_Code.ino
@@ -2,8 +2,8 @@
 long duration;
     int distance;
     
-    const int trig = 9;
-    const int echo = 10;
+    const int trig = 9; //sets trig in pin9
+    const int echo = 10;  //sets echo in pin 10
    
     
     void setup() {
